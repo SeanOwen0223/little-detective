9dac98061218aefc64de06a6e6890fd94e880540
Sean Owen Refolledo <seanowenrefolledo@gmail.com>
2026-03-10 23:24:07 +0800

    Fix: added base path for deployment

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/little-detective/', // <--- MAKE SURE THIS MATCHES YOUR REPO NAME
   plugins: [
     react(),
     tailwindcss(),
